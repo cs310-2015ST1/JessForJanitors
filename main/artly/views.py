132e3ac54d649d668b55e5f6520d6da1eb3950db
Sara T <saratan@mac.com>
2015-06-08 21:37:54 -0700

    created population script, populated database, updated index.html to reflect populated database

--- main/artly/views.py
+++ main/artly/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from artly.models import ArtInstallation
 
 def index(request):
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by name
+    # Place the list in our context_dict dictionary which will be passed to the template engine.
+    art_list = ArtInstallation.objects.order_by('-name')
+    context_dict = {'artinstallations': art_list}
 
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
-
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-
+    # Render the response and send it back!
     return render(request, 'artly/index.html', context_dict)
