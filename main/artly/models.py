8ffa64e26ebeb48a48ea43d278faadfd47340a22
erik <ehauner@gmail.com>
2015-06-10 11:41:24 -0700

    Added the populate script for real this time

--- main/artly/models.py
+++ main/artly/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class ArtInstallation(models.Model):
     name = models.CharField(max_length=128, unique=True)
     url = models.URLField(default="http://www.google.com/")
+    lat = models.FloatField(default=0.0)
+    lon = models.FloatField(default=0.0)
 
     def __unicode__(self):  #For Python 2, use __str__ on Python 3
         return self.name
