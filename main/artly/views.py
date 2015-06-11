5dd988bc3f6715da27a325bc55792d61b0bbafe8
erik <ehauner@gmail.com>
2015-06-10 21:51:32 -0700

    Changed order of sites to ascending order

--- main/artly/views.py
+++ main/artly/views.py
@@ -6,7 +6,7 @@ def index(request):
     # Query the database for a list of ALL categories currently stored.
     # Order the categories by name
     # Place the list in our context_dict dictionary which will be passed to the template engine.
-    art_list = ArtInstallation.objects.order_by('-name')
+    art_list = ArtInstallation.objects.order_by('name')
     context_dict = {'artinstallations': art_list}
 
     # Render the response and send it back!
