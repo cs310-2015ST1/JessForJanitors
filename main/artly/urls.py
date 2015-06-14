f2e472faa736a344c2588e3fb260eaf8a3a3acbb
erik <ehauner@gmail.com>
2015-06-13 22:14:22 -0700

    Updated the map link to show placemarker, added more urls for adding and removing sites from list of those selected in urls.py

--- main/artly/urls.py
+++ main/artly/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, url
 from artly import views
 
-urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.index, name='index'),
+    url(r'^add_site/$', 'add_site'),
+    url(r'^remove_site/$', 'remove_site'),
+    )
