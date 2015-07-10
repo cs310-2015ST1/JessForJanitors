8e6df50128f2ee8979b5b5b9cd5a5a8e11778c7f
Jovy Eramela <z2v9a@ugrad.cs.ubc.ca>
2015-07-10 10:40:49 -0700

    First attempt at Favourite method in views.py (mirroring click_installation)

--- main/artly/models.py
+++ main/artly/models.py
@@ -6,7 +6,8 @@ class ArtInstallation(models.Model):
     url = models.URLField(default="http://www.google.com/")
     lat =  models.FloatField(default=49.2508548)
     lon = models.FloatField(default=-123.1174762)
-    selected = models.BooleanField(default =False)
+    selected = models.BooleanField(default=False)
+    favourited = models.BooleanField(default=False)
 
     def __unicode__(self):  #For Python 2, use __str__ on Python 3
         return self.name
@@ -15,5 +16,5 @@ class UserInformation(models.Model):
     userid = models.CharField(max_length=128, unique=True)
     username = models.CharField(max_length=128)
     password = models.CharField(max_length=128)
-    favouriteinstallations = models.CommaSeparatedIntegerField(max_length=512)
+    savedinstallations = models.CommaSeparatedIntegerField(max_length=512)
     
