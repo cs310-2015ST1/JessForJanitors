9028e3319be70c5cc77ca98f4f697a899e3469a2
Stephen Kan <stebokan@gmail.com>
2015-07-15 10:29:06 -0700

    Finetuning login more to have new fields and editable Admin Page access

--- main/artly/admin.py
+++ main/artly/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from artly.models import ArtInstallation
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 admin.site.register(ArtInstallation)
+
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class UserInline(admin.StackedInline):
+    model = ArtlyUser
+    can_delete = False
+
+# Define a new User admin
+class UserAdmin(UserAdmin):
+    inlines = (UserInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
