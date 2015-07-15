320c1c9c0ac655d06253e793de0dd837109c23d7
erik <ehauner@gmail.com>
2015-07-15 01:12:40 -0700

    Made heart change icon when row selected

--- main/static/js/artly-ajax.js
+++ main/static/js/artly-ajax.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
 
+    var checked;
+
     $(".table-hover tr").click(function (event) {
+
         if (event.target.tagName !== 'SPAN' && event.target.tagName !== 'INPUT') {
+
             $(':checkbox', this).trigger('click');
+
+            if(checked) {
+                $(".heart", this).addClass('glyphicon-heart').removeClass('glyphicon-heart-empty');
+            } else {
+                $(".heart", this).addClass('glyphicon-heart-empty').removeClass('glyphicon-heart');
+            }
 
             var installation_name;
 
@@ -12,7 +22,7 @@ $(document).ready(function() {
                /*Return name back just for testing purposes*/
                $('#installation_name').html(data);
             });
-//            
+//
 //            $.get('/artly/click_favourite/', {name: installation_name}, function(data){
 //                /*Return name back just for testing purposes*/
 //                $('#installation_name').html(data);
@@ -23,8 +33,10 @@ $(document).ready(function() {
     $("input[type='checkbox']").change(function (e) {
         if ($(this).is(":checked")) {
             $(this).closest('tr').addClass("success");
+            checked = true;
         } else {
             $(this).closest('tr').removeClass("success");
+            checked = false;
         }
 
         sortCheck();
