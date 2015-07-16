c74ff9e6a1d352e218ba0f4cf8d91e15547b49a7
erik <ehauner@gmail.com>
2015-07-15 22:30:24 -0700

    Made selected placemarkers appear highlighted

--- main/static/js/artly-ajax.js
+++ main/static/js/artly-ajax.js
@@ -2,16 +2,22 @@ $(document).ready(function() {
 
     var checked;
 
+    var index;
+
     $(".table-hover tr").click(function (event) {
 
         if (event.target.tagName !== 'SPAN' && event.target.tagName !== 'INPUT') {
 
             $(':checkbox', this).trigger('click');
 
+            index = parseInt($(':checkbox', this).attr("data-installation-id").substring(4) - 1);
+
             if(checked) {
                 $(".heart", this).addClass('glyphicon-heart').removeClass('glyphicon-heart-empty');
+                markers[index].setIcon(highlightedIcon());
             } else {
                 $(".heart", this).addClass('glyphicon-heart-empty').removeClass('glyphicon-heart');
+                markers[index].setIcon(normalIcon());
             }
 
             var installation_name;
@@ -42,4 +48,21 @@ $(document).ready(function() {
 
         sortCheck();
     });
+
+    $('#markers_info .marker').hover(
+      // mouse in
+      function () {
+        // first we need to know which <div class="marker"></div> we hovered
+        var index = $('#markers_info .marker').index(this);
+        markers[index].setIcon(highlightedIcon());
+      },
+      // mouse out
+      function () {
+        // first we need to know which <div class="marker"></div> we hovered
+        var index = $('#markers_info .marker').index(this);
+        markers[index].setIcon(normalIcon());
+      }
+
+    );
+
 });
