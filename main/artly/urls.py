cdf9b76f9dc7ef58412ebe5ba1701298163a61bf
Stephen Kan <stebokan@gmail.com>
2015-07-17 11:12:58 -0700

    Fixed bugs with favouriting

--- main/artly/urls.py
+++ main/artly/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns, url
 from artly import views
 from artly import populate_artly
-from artly import toggle_favourites
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^map/$', views.map, name='map'),
     url(r'^populate/$', populate_artly.populate, name='populate'),
-    url(r'^toggle_favourites/$', toggle_favourites.toggle_favourites, name='toggle_favourites'),
+    url(r'^toggle_favourite/$', views.toggle_favourite, name='toggle_favourite'),
     )
