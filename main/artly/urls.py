bf6b64c4d42b3bd1b0291f873e3f1ce9b34954d3
Stephen Kan <stebokan@gmail.com>
2015-07-16 21:34:22 -0700

    Favourites button gets location ids.

--- main/artly/urls.py
+++ main/artly/urls.py
@@ -6,6 +6,4 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^map/$', views.map, name='map'),
     url(r'^populate/$', populate_artly.populate, name='populate'),
-    url(r'^click_installation/$', views.click_installation, name='click_installation'),
-    url(r'^click_save/$', views.click_save, name='click_save'),
     )
